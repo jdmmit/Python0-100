c12cee1987e70fa7b4040d45253f131baebf8a5a
Juan David Murillo Mejia <juandavidmurillomejia0718@outlook.es>
2025-05-19 21:00:25 -0500

    Add files via upload

---
+++ Conceptos_Basicos/02_operadores.py
@@ -0,0 +1,45 @@
+### Operadores ####
+
+# Operadores Aritméticos
+
+print(2 + 3) # Suma
+print(2 - 3) # Resta
+print(2 * 3) # Multiplicación
+print(2 / 3) # División
+print(2 // 3) # División entera
+print(2 % 3) # Módulo
+print(2 ** 3) # Potencia
+
+
+# Operadores de Comparación
+
+print(2 == 3) # Igual
+print(2 != 3) # Diferente
+print(2 > 3) # Mayor
+print(2 < 3) # Menor
+print(2 >= 3) # Mayor o igual
+print(2 <= 3) # Menor o igual
+
+
+
+# Operadores Lógicos
+
+print(True and False) # Y
+print(True or False) # O
+print(not True) # No
+
+
+# Operadores de Asignación
+
+x = 5 # Asignación
+x += 5 # Asignación con suma
+x -= 5 # Asignación con resta
+x *= 5 # Asignación con multiplicación
+x /= 5 # Asignación con división
+x //= 5 # Asignación con división entera
+x %= 5 # Asignación con módulo
+x **= 5 # Asignación con potencia
+
+
+
+
---
+++ Conceptos_Basicos/03_strings.py
@@ -0,0 +1,16 @@
+### Strings ###
+
+my_string = "Mi string"
+mu_other_string = "Mi otro string"
+
+print(len(my_string))  # Length of the string
+print(my_string + " " + mu_other_string)  # Concatenation of strings
+
+my_new_string = "este es un string \n con salto de linea"
+print(my_new_string)  # Print the string with a newline character
+
+my_tab_string = "este es un string \t con tabulacion"
+print(my_tab_string)  # Print the string with a tab character
+
+my_scape_string = "\\ este es un string \\ con barra invertida"
+print(my_scape_string)  # Print the string with a backslash character
